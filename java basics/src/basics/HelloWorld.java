051caf378e50edcd851e0c12cc51a8a239532590
lenovo <lenovo@DESKTOP-C1LA7E7>
2021-10-19 15:21:49 +0530

    testing push

--- java basics/src/basics/HelloWorld.java
+++ java basics/src/basics/HelloWorld.java
@@ -2,7 +2,7 @@ package basics;
 
 public class HelloWorld {
      public static void main(String[] args) {
-    	 System.out.println("HelloWorld");
+    	 System.out.println("HelloWorld dxc mates");
     	 
      }
 
