6b606e6d0466b2da0b0147dd7879654a38d8c245
lenovo <lenovo@DESKTOP-C1LA7E7>
2021-10-20 10:40:54 +0530

    objectclass

--- java basics/src/objectclass/OddEven.java
+++ java basics/src/objectclass/OddEven.java
@@ -7,13 +7,13 @@ public class OddEven {
 		Scanner scan = new Scanner(System.in);  
 		System.out.print("Enter the number: ");
 		int num = scan.nextInt();  
-		findoddeven(15);
+		findoddeven(num);
 		
 		
 	}
 
 	private static void findoddeven(int number) {
-		if(number%3 == 0) {
+		if(number%4 == 0) {
 			System.out.println("is even no");
 		}
 		else {
